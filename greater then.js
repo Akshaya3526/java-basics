6b4cdd987296c4e214bfe3cf23c2ac2e11ca8956
Akshaya3526 <akshaya352006@gmail.com>
2025-07-30 19:14:03 +0530

    Create greater then.js

---
+++ greater then.js
@@ -0,0 +1,15 @@
+public class GreatestNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        if (a > b && a > c) {
+            System.out.println("a is the greatest" );
+        } else if (b > c) {
+            System.out.println("b is the greatest");
+        } else {
+            System.out.println("c is the greatest");
+        }
+    }
+}
